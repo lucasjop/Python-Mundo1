0159a6376cfc8847e7a84fe4e3ce373d2c68ada6
lucasjop <lucasjop@gmail.com>
2022-01-28 23:59:48 -0300

    More exercises
    Adição dos exercícios 02 e 03

---
+++ ex002.py
@@ -0,0 +1,3 @@
+print('===== Exercício 02 =====')
+nome = input('Digite seu nome: ')
+print(f'É um prazer te conhecer, {nome}!')
---
+++ ex003.py
@@ -0,0 +1,6 @@
+print('===== Exercício 03 =====')
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+print('A soma entre', n1, 'e', n2, 'é', n1+n2)
+print('A soma entre {} e {} vale {}'.format(n1, n2, n1+n2))
+print(f'A soma entre {n1} e {n2} = {n1+n2}')
