0d09b42e27a3d296e100d4f3be81b0f458acbc30
lucasjop <lucasjop@gmail.com>
2022-01-29 23:55:20 -0300

    Exercises 004-006
    Exercises 004-006

---
+++ ex005.py
@@ -0,0 +1,3 @@
+print('===== Exercício 05 =====')
+n1 = int(input('Digite um número: '))
+print(f'O antecessor de {n1} é {n1-1} e o sucessor é {n1+1}')
---
+++ ex006.py
@@ -0,0 +1,4 @@
+print('===== Exercício 06 =====')
+n1 = int(input('Digite um número: '))
+print(f'O dobro de {n1} é {2*n1}, o triplo é {3*n1} e sua raiz quadrada é {pow(n1, (1/2))}')
+print('O dobro de {} é {}, o triplo é {} e sua raiz quadrada é {: .2f}'.format(n1, 2*n1, 3*n1, pow(n1, (1/2))))
