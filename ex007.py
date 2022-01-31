29b2cc4337483a2e3329fc885eea885b89ddf373
lucasjop <lucasjop@gmail.com>
2022-01-30 22:57:23 -0300

    Exercises 007-009
    Adicionados os exercícios 007-009

---
+++ ex007.py
@@ -0,0 +1,5 @@
+print('===== Exercício 07 =====')
+nome = input('Digite seu nome: ')
+n1 = int(input('Digite a primeira nota: '))
+n2 = int(input('Digite a segunda nota: '))
+print(f'Parabéns {nome}, suas notas são {n1} e {n2}. Sua média é {(n1+n2)/2}')
---
+++ ex008.py
@@ -0,0 +1,4 @@
+print('===== Exercício 08 =====')
+n1 = int(input('Digite o valor em metros: '))
+print(f'{n1}m = {100*n1}cm', end=' e ')
+print(f'{n1}m = {1000*n1}mm')
